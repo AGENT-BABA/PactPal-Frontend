554574a195a1c4ff6b669228e5d0e7e17bf5b3f7
Krushna Sonawane <skrushna1983@gmail.com>
2025-09-14 23:35:37 +0530

    Debugging

--- src/config/api.js
+++ src/config/api.js
@@ -5,5 +5,6 @@ export const API_ENDPOINTS = {
   SIMPLIFY_TEXT: `${API_BASE_URL}/api/simplify-text`,
   EXTRACT_CLAUSES: `${API_BASE_URL}/api/extract-clauses`,
 };
+console.log("API BASE URL:", API_BASE_URL);
 
 export default API_ENDPOINTS;
